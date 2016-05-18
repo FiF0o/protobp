f525663d4aee9ebcc3e1edeb649afedab3f6842f
jon <jonathan.lazarini@wipro.com>
2016-05-18 20:00:06 +0100

    remove CDN and use browserify for loading dependancies, fix jquery bug when loading bootstrap

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,15 @@
-import $ from '../vendors/jquery/dist/jquery.js';
+//import $ from '../vendors/jquery/dist/jquery.js';
 //import Boot from '../vendors/bootstrap/dist/js/bootstrap.js';
 
+var QC = window.$ = window.jQuery = require('./../vendors/jquery/dist/jquery.js');
 //// * Debug * //
-console.log($);
-//console.log(Boot);
+console.log('QC');
+console.log(QC);
+
+var Boot = require('./../vendors/bootstrap/dist/js/bootstrap.js');
+//console.log($);
+console.log('Boot');
+console.log(Boot);
 
 import kikou from './test';
 console.log('kikou');
