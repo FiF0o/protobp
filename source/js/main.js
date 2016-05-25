9e22208855ac05780bd634c53375d011b1a55516
jon <jonathan.lazarini@wipro.com>
2016-05-25 13:06:44 +0100

    finish homepage

--- source/js/main.js
+++ source/js/main.js
@@ -3,23 +3,26 @@
 
 var QC = window.$ = window.jQuery = require('./../vendors/jquery/dist/jquery.js');
 //// * Debug * //
-console.log('QC');
-console.log(QC);
+//console.log('QC');
+//console.log(QC);
 
 var Boot = require('./../vendors/bootstrap/dist/js/bootstrap.js');
 //console.log($);
-console.log('Boot');
-console.log(Boot);
+//console.log('Boot');
+//console.log(Boot);
 
 import kikou from './test';
-console.log('kikou');
-console.log(kikou);
+//console.log('kikou');
+//console.log(kikou);
 
 
 $('#change_management').click(function() {
     console.log('div clicked!');
 });
 
-(function(){
-    console.log('pure js iife');
-})();
+(function(yo){
+    //console.log('pure js iife');
+    //console.log(Boot);
+    //console.log('yo');
+    //console.log(yo);
+})(Boot);
